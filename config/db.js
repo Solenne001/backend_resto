04c74ed16e2df7f1c0b0ac8bc4ceb80ec2061b50
Solenne <tomsosso2@gmail.com>
2025-10-08 15:25:33 +0000

    Connect backend to Railway MySQL

--- config/db.js
+++ config/db.js
@@ -1,18 +1,26 @@
-// server/config/db.js
+// // server/config/db.js
+// import mysql from "mysql2/promise";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// export const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// export default db;
+
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const connection = await mysql.createConnection(process.env.DATABASE_URL);
 
-export default db;
-
+export default connection;
